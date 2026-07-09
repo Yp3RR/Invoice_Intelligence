8937855a4a11737309b6b6f8f5c6804078a2a7e3
Yash Patil <yashpatil1492@gmail.com>
2026-07-09 13:55:43 +0530

    Loaded the API key for its use using python-dotenv

--- src/extraction/llm_extractor.py
+++ src/extraction/llm_extractor.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-api_KEY = os.getenv("GROQ_API_KEY")
+API_KEY = os.getenv("GROQ_API_KEY")
 
 def extract_text_from_pdf(invoice_pdf):
     with pp.open(invoice_pdf) as pdf:
